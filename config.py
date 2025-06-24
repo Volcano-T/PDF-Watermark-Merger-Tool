977d4e230dfed4f6461583e10c4738a4ec6ce039
Tanzim Tomal <128964744+Volcano-T@users.noreply.github.com>
2025-06-24 10:08:57 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,37 @@
+from pdf_processor import PDFProcessor
+
+settings = {
+    "files": [],
+    "page_size": "",
+    "margin": "",
+
+    "watermark": {
+        "type": "Text",
+        "text": "Sample Watermark",
+        "image_path": None,
+        "font_size": 16,
+        "opacity": 0.3,
+        "rotation": 0,
+        "position": "Center",
+        "page_range": "All"
+    },
+
+    "signature": {
+        "image_path": None,
+        "position": "Bottom-Right",
+        "scale": 0.3,
+        "rotation": 0,
+        "page_range": "All"
+    },
+
+    "header_footer": {
+        "header_text": "",
+        "footer_text": "",
+        "font_size": 12,
+        "page_range": "All",
+        "include_time": False,
+        "include_date": False,
+        "include_page_number": False
+    },
+    "processor": PDFProcessor(),
+}
